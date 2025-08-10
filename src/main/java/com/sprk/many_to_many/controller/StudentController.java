b4dbc58f7f05a0335e8d4e66884e217cf5b35f24
Gani25 <memonabdulgani3d@gmail.com>
2025-08-10 13:17:28 +0530

    Delete course by student id

--- src/main/java/com/sprk/many_to_many/controller/StudentController.java
+++ src/main/java/com/sprk/many_to_many/controller/StudentController.java
@@ -5,6 +5,7 @@ import com.sprk.many_to_many.entity.Student;
 import com.sprk.many_to_many.repository.CourseRepository;
 import com.sprk.many_to_many.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -43,5 +44,39 @@ public class StudentController {
 
         }
         return savedStudent;
+    }
+    // Get Student By Id
+    @GetMapping("/student/{id}")
+    public Student getStudent(@PathVariable int id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/delete_course/{rollNo}/{courseId}")
+    public ResponseEntity<?> deleteCourse(@PathVariable int rollNo, @PathVariable int courseId) {
+        Student dbStudent = studentRepository.findById(rollNo).orElse(null);
+        Course dbCourse = courseRepository.findById(courseId).orElse(null);
+
+        Student savedStudent = null;
+        if(dbStudent != null && dbCourse != null) {
+            // First check if that student have that course or not
+            List<Course> studentCourses = dbStudent.getCourses();
+            if(studentCourses.size() == 0){
+
+                    return ResponseEntity.status(400).body("Student with rollno = "+rollNo+" have no courses enrolled");
+
+            }
+            else if(studentCourses != null) {
+                if(studentCourses.contains(dbCourse)) {
+                    studentCourses.remove(dbCourse);
+                    dbStudent.setCourses(studentCourses);
+                    savedStudent =  studentRepository.save(dbStudent);
+                }
+                else{
+                    return ResponseEntity.status(404).body("Student with rollNo = "+rollNo+" have no course with courseId = "+courseId+" associated");
+                }
+            }
+
+        }
+        return ResponseEntity.ok().body(savedStudent);
     }
 }
--- src/main/java/com/sprk/many_to_many/entity/Student.java
+++ src/main/java/com/sprk/many_to_many/entity/Student.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -28,9 +29,25 @@ public class Student {
     @JoinTable(
             name = "student_course",
             joinColumns = @JoinColumn(name = "roll_no"),
-            inverseJoinColumns = @JoinColumn(name="course_id")
+            inverseJoinColumns = @JoinColumn(name = "course_id")
     )
 //    @JsonManagedReference
     private List<Course> courses;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Student)) {
+            return false;
+        }
+        Student student = (Student) o;
+        return rollNo == student.rollNo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(rollNo);
+    }
 }
